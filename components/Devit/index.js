b878c56b43dc14139c414b7e26df0855d8ac41fc
snc0lt <xjinxer@gmail.com>
2020-12-17 10:06:06 -0500

    firebase-admin

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -1,20 +1,41 @@
 import Avatar from "components/Avatar"
 import useTimeAgo from "hooks/useTimeAgo"
+import useDateTimeFormat from "hooks/useDateTimeFormat"
+import Link from "next/link"
+import { useRouter } from "next/router"
 
-export default function Devit({ avatar, userName, content, createdAt, img }) {
-
+export default function Devit({
+  avatar,
+  userName,
+  content,
+  createdAt,
+  img,
+  id,
+}) {
   const timeago = useTimeAgo(createdAt)
+  const createdAtFormated = useDateTimeFormat(createdAt)
+  const router = useRouter()
+
+  const handleArticleClick = (e) => {
+    e.preventDefault()
+    router.push("/status/[id]", `/status/${id}`)
+  }
+
   return (
     <>
-      <article>
+      <article onClick={handleArticleClick}>
         <div>
           <Avatar alt={userName} src={avatar} />
         </div>
         <section>
           <header>
-          <strong>{userName}</strong>
-            <span>·</span>
-            <date>{timeago}</date>
+            <strong>{userName}</strong>
+            <span> · </span>
+            <Link href={`/status/[id]`} as={`/status/${id}`}>
+              <a>
+                <time title={createdAtFormated}>{timeago}</time>
+              </a>
+            </Link>
           </header>
           <p>{content}</p>
           {img && <img src={img} />}
@@ -22,9 +43,19 @@ export default function Devit({ avatar, userName, content, createdAt, img }) {
       </article>
       <style jsx>{`
         article {
-          border-bottom: 2px solid #eaf7ff;
+          border-bottom: 1px solid #eee;
           display: flex;
           padding: 10px 15px;
+        }
+        article:hover {
+          background: #f5f8fa;
+          cursor: pointer;
+        }
+        img {
+          border-radius: 10px;
+          height: auto;
+          margin-top: 10px;
+          width: 100%;
         }
         div {
           padding-right: 10px;
@@ -33,18 +64,13 @@ export default function Devit({ avatar, userName, content, createdAt, img }) {
           line-height: 1.3125;
           margin: 0;
         }
-        span {
-          margin: 0 5px;
-        }
-        date {
+        a {
           color: #555;
           font-size: 14px;
+          text-decoration: none;
         }
-        img {
-          border-radius: 10px;
-          height: auto;
-          width: 100%;
-          margin-top: 10px;
+        a:hover {
+          text-decoration: underline;
         }
       `}</style>
     </>
